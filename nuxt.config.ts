085cd4e82ca4c5eb8f58d2a1268a8e22f2479f34
Jan Kuna <jkuna@denevy.eu>
2023-03-17 22:36:37 +0100

    Add layouts and components

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: ['@nuxtjs/tailwindcss'],
+    layout: 'default',
     css: ['~/assets/css/main.css'],
     postcss: {
         plugins: {
